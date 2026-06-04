8599df25ef51effb0b25696b8b8c6f61a47e1a0c
Vinxkumar <vinxkumar178@gmail.com>
2026-06-04 12:51:00 +0530

    Minor updates

--- src/components/DocumentsComponent/GettingStartedComponents/GettingStarted.tsx
+++ src/components/DocumentsComponent/GettingStartedComponents/GettingStarted.tsx
@@ -10,35 +10,36 @@ import {
   TbHeadset 
 } from "react-icons/tb";
 
+// gettingStartedItems — change to absolute paths
 export const gettingStartedItems = [
   {
     label: "Overview",
-    path: "overview",
-    icon: <TbChartPie/>
+    path: "/docs/getting-started/overview",
+    icon: <TbChartPie />
   },
   {
     label: "Get started for users",
-    path: "get-started-for-users",
+    path: "/docs/getting-started/get-started-for-users",
     icon: <TbRocket />
   },
   {
     label: "Tutorials",
-    path: "tutorials",
+    path: "/docs/getting-started/tutorials",
     icon: <TbBook />
   },
   {
     label: "Concepts for administrators",
-    path: "concepts-for-admins",
+    path: "/docs/getting-started/concepts-for-admins",
     icon: <TbShieldCheck />
   },
   {
     label: "Sample data",
-    path: "sample-data",
+    path: "/docs/getting-started/sample-data",
     icon: <TbDatabase />
   },
   {
     label: "Contact us",
-    path: "contact-us",
-    icon: <TbHeadset/>
-  }
+    path: "/docs/getting-started/contact-us",
+    icon: <TbHeadset />
+  },
 ];
