bc877581bf14b57367453cbea12cc5b22cf28d72
aksharkh <aksharkh04@gmail.com>
2026-06-05 13:08:34 +0530

    corrected production error in vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { CompanyProfile } from './pages/CompanyProfile';
 
